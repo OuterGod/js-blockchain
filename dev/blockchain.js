9643deabdde2cc3dc6d2ca1195f5743de360a563
OuterGod <jamargareto@gmail.com>
2018-11-21 00:57:20 +0100

    hashing function

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,40 +1,46 @@
-
+const sha256 = require('sha256');
 
 function Blockchain() {
-  this.chain = [];
-  this.newTransactions = [];
+    this.chain = [];
+    this.newTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
-  const newBlock = {
-    index: this.chain.length + 1,
-    timestamp: Date.now(),
-    transactions: this.newTransactions,
-    nonce: nonce,
-    hash: hash,
-    previousBlockHash: previousBlockHash
-  };
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce: nonce,
+        hash: hash,
+        previousBlockHash: previousBlockHash
+    };
 
-  this.newTransactions = [];
-  this.chain.push(newBlock);
+    this.newTransactions = [];
+    this.chain.push(newBlock);
 
-  return newBlock;
+    return newBlock;
 }
 
 Blockchain.prototype.getLastBlock = function () {
-  return this.chain[this.chain.length - 1];
+    return this.chain[this.chain.length - 1];
 }
 
 Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
-  const newTransaction = {
-    amount: amount,
-    sender: sender,
-    recipient: recipient
-  };
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
 
-  this.newTransactions.push(newTransaction);
+    this.newTransactions.push(newTransaction);
 
-  return this.getLastBlock()['index'] + 1;
+    return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
 }
 
 module.exports = Blockchain;
