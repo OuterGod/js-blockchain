3e5a7575af20c8e801aaa3b268fc37c2e66a4727
OuterGod <jamargareto@gmail.com>
2018-11-21 11:46:30 +0100

    testing pow

---
+++ dev/test_pow.js
@@ -0,0 +1,25 @@
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+const previousBlockHash = 'woiefwoiefj3029j0923fj';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'alex0823J0I9H4FH34',
+        recipient: 'jennFIJ2309JF20J'
+    },
+    {
+        amount: 100,
+        sender: 'alex0823J0I9H4FH34',
+        recipient: 'jennFIJ2309JF20J'
+    },
+    {
+        amount: 25,
+        sender: 'jennFIJ2309JF20J',
+        recipient: 'alex0823J0I9H4FH34'
+    },
+];
+
+console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+
+//console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 92693));
